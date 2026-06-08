bc4eed9319df8f28cd74ecba985fc73afea8f568
jeisson <jeissoncortes02@outlook.com>
2026-06-08 17:51:14 -0500

    Initial commit

---
+++ src/main/java/com/jeisson/vectorsearchengine/VectorSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.jeisson.vectorsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VectorSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeisson/vectorsearchengine/VectorSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeisson.vectorsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
